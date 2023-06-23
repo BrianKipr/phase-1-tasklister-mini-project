3e0d88737ebcd064b226ac6144282de7b847e5cc
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-23 21:13:15 +0300

    main

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const form = document.getElementById("create-task-form");
+const taskList = document.getElementById("tasks");
+
+form,addEventListener("submit", function(e){
+  e.preventDefault();
+
+  const input = document.getElementById('new-task-description');
+  const taskDescription = input.value;
+
+  const listItem = document.createElement("li")
+  listItem.textContent = taskDescription;
+
+
+  taskList.appendChild(listItem);
+
+  input.value = ""
+});
+
+
+
+
 });
